c4c950c94026f73a09c6f3a85d0ded4786ca6e84
Mike Delmonaco <mthundershock@gmail.com>
2024-05-04 20:13:50 -0400

    progress on operators

--- 2024/interpreter/main.py
+++ 2024/interpreter/main.py
@@ -1,4 +1,16 @@
+import os
 from manim import *
+import sys
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+def local_path(path: str) -> str:
+    return os.path.join(script_dir, path)
+
+monospace = "Monospace"
+if sys.platform == 'win32':
+    monospace = 'consolas'
+elif sys.platform == 'darwin':
+    monospace = 'monaco'
 
 class Title(Scene):
     def construct(self):
@@ -22,6 +34,16 @@ class Intro(Scene):
             MathTex(r'factorial(4)'),
         ).arrange(DOWN, aligned_edge=LEFT)
         indented.shift(RIGHT)
+        factorial = Tex(r'''
+                        \begin{verbatim}
+                        letrec factorial = function (n) =>
+                          if n == 0
+                          then 1
+                          else n * factorial(n - 1)
+                        in
+                        factorial(4)
+                        \end{verbatim}
+                        ''')
         self.play(Write(factorial))
         self.wait(1)
         self.play(Unwrite(factorial))
@@ -34,17 +56,17 @@ class Constants(Scene):
         self.wait(1)
         self.play(Unwrite(title))
 
-        numbers = VGroup(MathTex('1'), MathTex('-12'), MathTex('3.14')).arrange(DOWN, buff=2)
-        booleans = VGroup(MathTex('\mathrm{true}'), MathTex('\mathrm{false}')).arrange(DOWN, buff=2)
+        numbers = VGroup(MathTex(r'\mathtt{1}'), MathTex(r'\mathtt{-12}'), MathTex(r'\mathtt{3.14}')).arrange(DOWN, buff=2)
+        booleans = VGroup(MathTex(r'\mathtt{true}'), MathTex(r'\mathtt{false}')).arrange(DOWN, buff=2)
         constants = VGroup(numbers, booleans).arrange(RIGHT, buff=2)
         self.play(Write(constants))
         self.wait(1)
         self.play(Unwrite(constants))
 
         formulas = [
-            MathTex('{{2 + 2}}'),
-            MathTex('\mathrm{eval}( {{2 + 2}} )'),
-            MathTex('4'),
+            MathTex(r'{{\mathtt{2 + 2}}}'),
+            MathTex(r'\mathrm{eval}( {{\mathtt{2 + 2}}})'),
+            MathTex(r'\mathtt{4}'),
         ]
 
         diagram = VGroup(
@@ -64,13 +86,13 @@ class Constants(Scene):
         self.wait(1)
         self.play(Unwrite(formulas[-1]), Uncreate(diagram))
 
-        two = MathTex('{{2}}')
+        two = MathTex(r'{{\mathtt{2}}}')
         self.play(Write(two))
         self.wait(1)
         formulas = [
             two,
-            MathTex('\mathrm{eval}( {{2}} )'),
-            MathTex('{{2}}'),
+            MathTex(r'\mathrm{eval}( {{\mathtt{2}}} )'),
+            MathTex(r'{{\mathtt{2}}}'),
         ]
         for a,b in zip(formulas,formulas[1::]):
             self.play(TransformMatchingTex(a,b))
@@ -78,18 +100,19 @@ class Constants(Scene):
         self.play(Unwrite(formulas[-1]))
 
         formulas = [
-            MathTex('{{true}}'),
-            MathTex('\mathrm{eval}( {{true}} )'),
-            MathTex('{{true}}'),
+            MathTex(r'{{\mathtt{true}}}'),
+            MathTex(r'\mathrm{eval}( {{\mathtt{true}}} )'),
+            MathTex(r'{{\mathtt{true}}}'),
         ]
         self.play(Write(formulas[0]))
         for a,b in zip(formulas,formulas[1::]):
             self.play(TransformMatchingTex(a,b))
         self.wait(1)
-        sunglasses = ImageMobject("images/sunglasses.png").scale(1).next_to(formulas[-1], DOWN * 2)
+        sunglasses = ImageMobject(local_path("images/sunglasses.png")).scale(1).next_to(formulas[-1], DOWN * 2)
         self.play(FadeIn(sunglasses))
         self.wait(1)
         self.play(Unwrite(formulas[-1]), FadeOut(sunglasses))
+        self.wait(1)
 
 class Operations(Scene):
     def construct(self):
@@ -99,26 +122,26 @@ class Operations(Scene):
         self.play(Unwrite(title))
 
         ops = VGroup(
-            MathTex('+'),
-            MathTex('-'),
-            MathTex('*'),
-            MathTex('/'),
+            MathTex(r'\mathtt{+}'),
+            MathTex(r'\mathtt{-}'),
+            MathTex(r'\mathtt{*}'),
+            MathTex(r'\mathtt{/}'),
         ).arrange(RIGHT, buff=2)
         self.play(Write(ops))
         self.wait(1)
         self.play(Unwrite(ops))
 
-        addition = MathTex('1 + 1')
+        addition = MathTex(r'\mathtt{1 + 1}')
         self.play(Write(addition))
         self.wait(1)
         self.play(Unwrite(addition))
 
         formulas = [
-            MathTex('{{E_1}} + {{E_2}}'),
-            MathTex('{{HUGE_1}} + {{HUGE_2}}'),
-            MathTex('{{E_1}} + {{E_2}}'),
-            MathTex('\mathrm{eval}( {{E_1}} {{+}} {{E_2}} )'),
-            MathTex('\mathrm{eval}( {{E_1}} ) {{+}} \mathrm{eval}( {{E_2}} )'),
+            MathTex(r'{{\mathtt{E_1}}} + {{\mathtt{E_2}}}'),
+            MathTex(r'{{\mathtt{HUGE_1}}} + {{\mathtt{HUGE_2}}}'),
+            MathTex(r'{{\mathtt{E_1}}} + {{\mathtt{E_2}}}'),
+            MathTex(r'\mathrm{eval}( {{\mathtt{E_1}}} {{\mathtt{+}}} {{\mathtt{E_2}}} )'),
+            MathTex(r'\mathrm{eval}( {{\mathtt{E_1}}} ) {{+}} \mathrm{eval}( {{\mathtt{E_2}}} )'),
         ]
         self.play(Write(formulas[0]))
         for a,b in zip(formulas, formulas[1::]):
@@ -134,22 +157,118 @@ class Operations(Scene):
         self.wait(1)
         self.play(Unwrite(formulas[-1]))
 
-        code = Text('eval(e1) + eval(e2)', font="consolas")
+        code = Text('eval(E1) + eval(E2)', font=monospace)
         self.play(Write(code))
         self.wait(1)
         self.play(Unwrite(code))
 
-        turtles = ImageMobject('images/turtles.png').scale(0.5)
+        turtles = ImageMobject(local_path('images/turtles.png')).scale(0.5)
         self.play(FadeIn(turtles))
         self.wait(1)
         self.play(FadeOut(turtles))
 
-        formula = MathTex(r'\text{eval}', '(', 'E_1', ')', '+', r'\text{eval}', '(', 'E_2', ')')
+        formula = MathTex(r'\text{eval}', '(', r'\mathtt{E_1}', ')', '+', r'\text{eval}', '(', r'\mathtt{E_2}', ')')
         formula_yellow = formula.copy()
         for tex in [r'\text{eval}', '+', '(', ')']:
             formula_yellow.set_color_by_tex(tex, YELLOW)
         self.play(Write(formula))
         self.wait(1)
         self.play(FadeIn(formula_yellow))
-        self.remove(formula)
         self.wait(1)
+        self.remove(formula)
+        self.play(Unwrite(formula_yellow))
+        self.wait(1)
+
+        formulas = [
+            MathTex(r'\mathtt{1 + {{true}}}'),
+            MathTex(r'\mathtt{1 + {{1}}}'),
+            MathTex(r'\mathtt{1 + {{true}}}'),
+        ]
+        self.play(Write(formulas[0]))
+        self.wait(1)
+        for a,b in zip(formulas, formulas[1::]):
+            self.play(TransformMatchingTex(a,b))
+            self.wait(1)
+        self.play(formulas[-1].animate.set_color(RED), Wiggle(formulas[-1]))
+        self.wait(1)
+        self.play(formulas[-1].animate.set_color(WHITE))
+        self.wait(1)
+        self.play(Unwrite(formulas[-1]))
+        self.wait(1)
+
+        formulas = [
+            MathTex(r'\mathrm{eval}(', r'\mathtt{2 * 3}', r'\mathtt{+}', r'\mathtt{10 / 5}', ')').set_color(YELLOW).set_color_by_tex_to_color_map({
+                r'\mathtt{2 * 3}': WHITE, r'\mathtt{+}': WHITE, r'\mathtt{10 / 5}': WHITE
+            }),
+            MathTex(r'\mathrm{eval}(', r'\mathtt{2 * 3}', ')', '+', r'\mathrm{eval}(', r'\mathtt{10 / 5}', ')').set_color(YELLOW).set_color_by_tex_to_color_map({
+                r'\mathtt{2 * 3}': WHITE, r'\mathtt{10 / 5}': WHITE
+            }),
+            MathTex('(', r'\mathrm{eval}(', r'\mathtt{2}', r')', '*', '\mathrm{eval}(', r'\mathtt{3}', ')', ')', '+', r'\mathrm{eval}(', r'\mathtt{10 / 5}', ')').set_color(YELLOW).set_color_by_tex_to_color_map({
+                r'\mathtt{2}': WHITE, r'\mathtt{3}': WHITE, r'\mathtt{10 / 5}': WHITE
+            }),
+            MathTex('(', r'\mathtt{2}', '*', r'\mathrm{eval}(', r'\mathtt{3}', ')', ')', '+', r'\mathrm{eval}(', r'\mathtt{10 / 5}', ')').set_color(YELLOW).set_color_by_tex_to_color_map({
+                r'\mathtt{3}': WHITE, r'\mathtt{10 / 5}': WHITE
+            }),
+            MathTex('(', r'\mathtt{2}', '*', r'\mathtt{3}', ')', '+', r'\mathrm{eval}(', r'\mathtt{10 / 5}', ')').set_color(YELLOW).set_color_by_tex_to_color_map({
+                r'\mathtt{10 / 5}': WHITE
+            }),
+            MathTex(r'\mathtt{6}', '+', r'\mathrm{eval}(', r'\mathtt{10 / 5}', ')').set_color(YELLOW).set_color_by_tex_to_color_map({
+                r'\mathtt{10 / 5}': WHITE
+            }),
+            MathTex(r'\mathtt{6}', '+', '(', r'\mathrm{eval}(', r'\mathtt{10}', ')', '/', r'\mathrm{eval}(', r'\mathtt{5}', ')', ')').set_color(YELLOW).set_color_by_tex_to_color_map({
+                r'\mathtt{10}': WHITE, r'\mathtt{5}': WHITE,
+            }),
+            MathTex(r'\mathtt{6}', '+', '(', r'\mathtt{10}', '/', r'\mathrm{eval}(', r'\mathtt{5}', ')', ')').set_color(YELLOW).set_color_by_tex_to_color_map({
+                r'\mathtt{5}': WHITE,
+            }),
+            MathTex(r'\mathtt{6}', '+', '(', r'\mathtt{10}', '/', r'\mathtt{5}', ')').set_color(YELLOW),
+            MathTex(r'\mathtt{6}', '+', r'\mathtt{2}').set_color(YELLOW),
+            MathTex(r'\mathtt{8}').set_color(YELLOW),
+        ]
+        self.play(Write(formulas[0]))
+        self.wait(1)
+        for a,b in zip(formulas, formulas[1::]):
+            self.play(TransformMatchingTex(a,b))
+            self.wait(1)
+        self.play(Unwrite(formulas[-1]))
+
+        formulas = [
+            MathTex(r'\mathrm{eval}', '(', r'\mathtt{1}', '/', r'\mathtt{0}', ')').set_color(YELLOW).set_color_by_tex_to_color_map({
+                r'\mathtt{1}': WHITE, '/': WHITE, r'\mathtt{0}': WHITE,
+            }),
+            MathTex(r'\mathrm{eval}', '(', r'\mathtt{1}', ')', '/', r'\mathrm{eval}', '(', r'\mathtt{0}', ')').set_color(YELLOW).set_color_by_tex_to_color_map({
+                r'\mathtt{1}': WHITE, r'\mathtt{0}': WHITE,
+            }),
+            MathTex(r'\mathtt{1}', '/', r'\mathrm{eval}', '(', r'\mathtt{0}', ')').set_color(YELLOW).set_color_by_tex_to_color_map({
+                r'\mathtt{0}': WHITE,
+            }),
+            MathTex(r'\mathtt{1}', '/', r'\mathtt{0}').set_color(YELLOW).set_color_by_tex_to_color_map({
+            }),
+        ]
+        self.play(Write(formulas[0]))
+        self.wait(1)
+        for a,b in zip(formulas, formulas[1::]):
+            self.play(TransformMatchingTex(a,b))
+            self.wait(1)
+        self.play(formulas[-1].animate.set_color(RED), Wiggle(formulas[-1]))
+        self.wait(1)
+        self.play(Unwrite(formulas[-1]))
+        self.wait(1)
+    
+    # TODO what if instead of doing it all in one line, while you do a sub-expression, you do that on a new line
+    # then it'll be like the call stack
+    # eval(1 + 2)
+    # eval(1) + eval(2)
+    # eval(1) + eval(2) \n eval(1)
+    # eval(1) + eval(2) \n 1
+    # 1 + eval(2)
+    # 1 + eval(2) \n eval(2)
+    # 1 + eval(2) \n 2
+    # 1 + 2
+    # 3
+    # also will avoid the weird stuff with the evals and parens not moving right
+
+
+class Testing(Scene):
+    def construct(self):
+        self.add(Tex(r'eval(\texttt{foo + bar})'))
